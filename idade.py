a963e4e119ed56969dc226cfb9da06e7662c065c
Ítalo Ryan Pimentel Horas <94705941+italopimentel@users.noreply.github.com>
2022-12-08 15:15:41 +0000

    idade update

--- idade.py
+++ idade.py
@@ -1,15 +1,16 @@
 
-idade = int(input("Digite uma idade: "))
 lista_idade = []
-while idade >= 0:
-    lista_idade.append(idade)
-    idade = int(input("Digite uma idade: "))
+idade = int(input("Digite uma idade: "))
+try:
+    while idade >= 0:
+        lista_idade.append(idade)
+        idade = int(input("Digite uma idade: "))
 
-contador = 0
-soma_idade = 0
+    soma_idade = 0
 
-for i in lista_idade:
-    soma_idade += i
-    contador += 1
+    for valor in lista_idade:
+        soma_idade += valor
 
-print("A média de idade foi: {}".format(soma_idade/contador))
+    print("A média de idade foi: {}".format(soma_idade/len(lista_idade)))
+except ZeroDivisionError:
+    print("O programa foi encerrado pois não foi digitado nenhum valor válido")
